b1510abf61e83cfa595a7a7b89bd9d6237b0dd07
chrisheo <jftj@naver.com>
2023-03-26 04:26:56 +0900

    choir : getSearch 코드 변경

--- model/foodSchema.js
+++ model/foodSchema.js
@@ -7,9 +7,13 @@ const food = new Schema({
     required: true,   // 필수 여부
     unique: true,     // 고유 값
   },
-  date: {
+  ordinary_date: {
     type: Number,
     required: true,
+
+  },
+  freeze_date: {
+    type: Number,
 
   },
   createdAt: {
