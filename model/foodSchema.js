25af7be0db963c63696acd96520d5083f8a9c1bd
seungryeol <opop0421@naver.com>
2023-03-25 22:29:24 +0900

    feat: make model

---
+++ model/foodSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const food = new Schema({
+  name: {
+    type: String,     // 자료형
+    required: true,   // 필수 여부
+    unique: true,     // 고유 값
+  },
+  date: {
+    type: Number,
+    required: true,
+
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Food', food);
