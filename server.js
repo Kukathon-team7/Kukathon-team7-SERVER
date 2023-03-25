2ff0074a47bcc1585e386933c77743ec89396719
seungryeol <opop0421@naver.com>
2023-03-25 16:26:10 +0900

    feat:몽고디비연결, add: 몽고디비라이브러리 추가

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.json({
-        success: true,
-    });
-});
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`server is listening at localhost:${process.env.PORT}`);
